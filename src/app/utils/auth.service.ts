f482290c9bac23e20ef3c7ef95ec9010325f5d4f
Luiz Antonio Vasconcelos de Moraes Mello Cavalcanti Negrinho <l.mello.cavalcanti@avanade.com>
2024-02-22 19:48:16 -0300

    persisted login implemented, root fontstyle defined

--- src/app/utils/auth.service.ts
+++ src/app/utils/auth.service.ts
@@ -1,4 +1,8 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from "@angular/core";
+import { environment } from "src/environments/environment";
+import { User } from './Interfaces/user-interface';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
@@ -7,14 +11,44 @@ import { Injectable } from "@angular/core";
 export class AuthService {
   private loggedIn = false;
   public username: string = '';
+  private apiUrl = environment.backend;
+  private session = 'Session';
+
+  constructor(private http: HttpClient, private router: Router){
+    this.loggedIn = !!sessionStorage.getItem(this.session);
+    const storedUsername = sessionStorage.getItem(this.session);
+    if (storedUsername !== null) {
+      this.username = storedUsername;
+    }
+  }
 
   login(username: string, password: string) {
-    this.username = username;
-    this.loggedIn = true;
+    const body = {
+      Username: username,
+      Password: password
+    }
+    return this.http.post<User>(this.apiUrl + "Login/Login", body).subscribe(
+      (response) => {
+        const user = response as User; // Type assertion para informar ao TypeScript que o objeto é do tipo User
+        if (user && user.id > 0) {
+          this.loggedIn = true;
+          this.username = user.name.split(' ')[0];
+          sessionStorage.setItem(this.session, this.username.split(' ')[0]);
+        } else {
+          console.log('Usuário ou Senha inválidos');
+          window.alert('Usuário ou senha inválidos!')
+        }
+      },
+      (error) => {
+        console.error('Erro ao fazer login:', error);
+      }
+    );
   }
 
   logout() {
     this.loggedIn = false;
+    sessionStorage.removeItem(this.session);
+    this.router.navigate(['/']);
   }
 
   isLoggedIn() {
